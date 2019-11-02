15e0a6de07119c171acf3bfb64e258527957056c
ElJocho <stier.jochen@web.de>
2019-11-02 16:47:46 +0100

    made test_area pylint proof

--- src/test_area.py
+++ src/test_area.py
@@ -11,107 +11,111 @@ import src.objects as objects
 
 class TestField(unittest.TestCase):
     """used for testing Field module"""
+    field = fld.Field()
+
     def test_init(self):
         """test if Field is succesfully initiated"""
-        f = fld.Field()
-        assert f.grid[49, 49] == 0
-        assert f.grid[49, 43] == 0
+        init_test_field = fld.Field()
+        assert init_test_field.grid[49, 49] == 0
+        assert init_test_field.grid[49, 43] == 0
         with self.assertRaises(IndexError):
-            f.grid[50, 49]
+            init_test_field.grid[50, 49]
 
-    def test_checkCell(self):
+    def test_check_cell(self):
         """check if Check Cell returns correct values"""
-        f = fld.Field()
-        assert f.check_cell([49, 49]) == 0
-        assert f.check_cell([3, 49]) == 0
+        assert self.field.check_cell([49, 49]) == 0
+        assert self.field.check_cell([3, 49]) == 0
         with self.assertRaises(IndexError):
-            f.check_cell([50, 1])
+            self.field.check_cell([50, 1])
 
-    def test_setCell(self):
+    def test_set_cell(self):
         """check if set_cell handles values correctly"""
-        f = fld.Field()
-        assert f.check_cell([49, 2]) == 0
-        f.set_cell([49, 2], 1)
-        assert f.check_cell([49, 2]) == 1
+        assert self.field.check_cell([49, 2]) == 0
+        self.field.set_cell([49, 2], 1)
+        assert self.field.check_cell([49, 2]) == 1
         with self.assertRaises(IndexError):
-            f.set_cell([69, 6], 1)
+            self.field.set_cell([69, 6], 1)
 
 
 class TestAnt(unittest.TestCase):
     """used for testing Ant module"""
+
+    ant = objects.Ant()
+    field = fld.Field()
+    ant.location = [10, 10]
+
     def test_init(self):
         """test if Ant is correctly initialized"""
-        a = objects.Ant()
-        assert a.alive is True
-        assert a.age == 0
-        assert a.location is None
-
-    a = objects.Ant()
-    a.location =[10, 10]
-    f = fld.Field()
+        assert self.ant.alive is True
+        assert self.ant.age == 0
+        assert self.ant.location == [10, 10]
 
     def test_die(self):
         """check if ants die if told so"""
-        assert self.a.alive is True
-        self.a.die()
-        assert self.a.alive is False
-        self.a.alive = True
+        assert self.ant.alive is True
+        self.ant.die()
+        assert self.ant.alive is False
+        self.ant.alive = True
 
-    def test_getOlder(self):
+    def test_get_older(self):
         """check if ants age the right way and die if they age 101 times"""
-        assert self.a.age == 0
+        assert self.ant.age == 0
         for age in range(1, 101):
-            self.a.get_older(self.f)
-            assert self.a.age == age
-            assert self.a.alive is True
-        self.a.get_older(self.f)
-        assert self.a.alive is False
-        self.a.alive = True
-        self.a.age = 0
+            self.ant.get_older(self.field)
+            assert self.ant.age == age
+            assert self.ant.alive is True
+        self.ant.get_older(self.field)
+        assert self.ant.alive is False
+        self.ant.alive = True
+        self.ant.age = 0
 
     def test_move(self):
         """test if ants move in the right direction and age afterwards"""
         foods = [objects.Food([0, 0])]
-        a2 = objects.Ant()
-        a2.location = [0, 0]
+        ant2 = objects.Ant()
+        ant2.location = [0, 0]
         hive = objects.Hive([12, 10])
-        ants = [self.a, a2]
-        self.a.move(ants, foods, hive, self.f)
-        assert self.a.age == 1
-        assert self.a.next == [9, 9]
-        self.a.has_food = True
-        self.a.move(ants, foods, hive, self.f)
-        assert self.a.age == 2
-        assert self.a.next == [11, 10]
+        ants = [self.ant, ant2]
+        self.ant.move(ants, foods, hive, self.field)
+        assert self.ant.age == 1
+        assert self.ant.next == [9, 9]
+        self.ant.has_food = True
+        self.ant.move(ants, foods, hive, self.field)
+        assert self.ant.age == 2
+        assert self.ant.next == [11, 10]
 
     def test_ant_names(self):
         """test if Ant names are requested correctly"""
-        pass
-        assert len(self.a.names) == 0
-        names = self.a.get_ant_names()
-        assert type(self.a.names) is list
-        assert len(self.a.names) == 40
-        assert self.a.name is None
-        self.a.set_name()
-        assert self.a.name is not None
+        if self.ant.names:
+            raise ValueError
+        self.ant.get_ant_names()
+        assert isinstance(self.ant.names, list)
+        assert len(self.ant.names) == 40
+        assert self.ant.name is None
+        self.ant.set_name()
+        assert self.ant.name is not None
 
 
 class TestFood(unittest.TestCase):
+    """testing class food"""
+    food = objects.Food()
+
     def test_food(self):
-        food = objects.Food()
-        food.amount = 20
-        food.location = [1, 2]
-        food.nom()
-        assert food.amount == 19
-        assert food.get_x() == food.location[0]
+        """test all important functions"""
+        self.food.amount = 20
+        self.food.location = [1, 2]
+        self.food.nom()
+        assert self.food.amount == 19
+        assert self.food.get_x() == self.food.location[0]
 
 
 class TestUtils(unittest.TestCase):
-    pass
+    """test utils"""
+
 
 class TestController(unittest.TestCase):
-    pass
+    """test controller"""
 
 
-if "__main__" == __name__:
+if __name__ == "__main__":
     unittest.main()
